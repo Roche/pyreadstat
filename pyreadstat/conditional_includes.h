54a9fdf3470d50100694c27aa338568fa1bd5e65
Todd <toddrme2178@gmail.com>
2019-07-25 14:54:33 -0400

    Make sure non-void function returns a value
    `_close` is an `int` type function, so it should return an `int` rather than returning nothing.  Otherwise you end up with a `no-return-in-nonvoid-function` issue.

--- pyreadstat/conditional_includes.h
+++ pyreadstat/conditional_includes.h
@@ -30,7 +30,6 @@
     int _SH_DENYWR; 
     int _SH_DENYRD;
     int _SH_DENYNO;
-    void assign_fd(void *io_ctx, int fd){};
-    int _close(int fd){};
+    int _close(int fd){ return 0; };
     
 #endif
