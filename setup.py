5c316637720b6733d98735b88c4f947812971a49
Otto Fajardo <otto.fajardob@gmail.com>
2021-05-05 11:10:37 +0200

    version 1.1.2

--- setup.py
+++ setup.py
@@ -136,7 +136,7 @@ short_description = "Reads and Writes SAS, SPSS and Stata files into/from pandas
 
 setup(
     name='pyreadstat',
-    version='1.1.1',
+    version='1.1.2',
     description=short_description,
     author="Otto Fajardo",
     author_email="pleasecontactviagithub@notvalid.com",
